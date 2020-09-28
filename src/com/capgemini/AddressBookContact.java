4019d19a0386a02b7a8c141bf6885eb748b778da
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-29 00:38:33 +0530

    [Ashutosh]Added:Multiple Address Book

--- src/com/capgemini/AddressBookContact.java
+++ src/com/capgemini/AddressBookContact.java
@@ -2,66 +2,55 @@ package com.capgemini;
 import java.util.*;
 public class AddressBookContact {
 
-	private ArrayList<AddressBookStructure> addressBook = new ArrayList<AddressBookStructure>();
+	public String bookname;
+	ArrayList<AddressBookStructure> addressBook = new ArrayList<AddressBookStructure>();
+ 
+ public AddressBookContact(String name) {
+	 bookname = name;
+ }
+ 
+ public void setAddressBook(ArrayList<AddressBookStructure> m) {
+	 this.addressBook= m;
+ }
+ public List<AddressBookStructure> getAddressBook(){
+	 return this.addressBook;
+ }
+ public void addContactDetails(AddressBookStructure ContactDetailsObj) {
+	 addressBook.add(ContactDetailsObj);
+ }
+ public List<AddressBookStructure> viewContactDetailss() {
+	 return addressBook;
+ }
+ public String updateContactDetails(AddressBookStructure a) {
+	 int y=0;
+	 AddressBookStructure b = new AddressBookStructure("", "", "", "", "", "", "", "");
+	 AddressBookStructure c = new AddressBookStructure("", "", "", "", "", "", "", "");
 	
-	
-	 public ArrayList<AddressBookStructure> getAddressBook(){
-		 return this.addressBook;
-	 }
-	
-	 public void addContact(AddressBookStructure contactObj) {
-		 addressBook.add(contactObj);
-	 }
-	
-	 public boolean check(String name)
-	 {
-		 for(AddressBookStructure x: addressBook) {
-			 if(x.getfirstname().equalsIgnoreCase(name)) 
-				 return true;
-		 }
-		 return false;
-	 }
-	 
-	 public ArrayList<AddressBookStructure> viewContactDetails() {
-		 return addressBook;
-	 }
-	 
-		 
-	 public String updateContact(AddressBookStructure a) {
-		 int y=0;
-		 AddressBookStructure b = new AddressBookStructure("", "", "", "", "", "", "", "");
-		 AddressBookStructure c = new AddressBookStructure("", "", "", "", "", "", "", "");
-		
-		 for(AddressBookStructure x: addressBook) {
-			 if(x.getfirstname().equalsIgnoreCase(a.getfirstname()) && x.getlastname().equalsIgnoreCase(a.getlastname())) {
+	 for(AddressBookStructure x: addressBook) {
+		 if(x.getfirstname().equalsIgnoreCase(a.getfirstname()) && x.getlastname().equalsIgnoreCase(a.getlastname())) {
 			 c=x;
-			 b=a;
-			 y++;
-			 }
+		 b=a;
+		 y++;
 		 }
-		 if(y==0)
-			 return "No contact found with this name";
-		 else {
-			 addressBook.remove(c);
-			 addressBook.add(b);
-			 return "Contact Updated";
-		 }
-			
 	 }
-	 public String deleteContact(String del) {
-		 
-			AddressBookStructure a=new AddressBookStructure("","","", "", "", "0","", "");
-			 for(AddressBookStructure x:addressBook) {
-				 if(x.getfirstname().equalsIgnoreCase(del)) {
-					 a=x;
-				 }
-			 }
-			 addressBook.remove(a);
-			 return "Delete success";
-				 
-			 }
-	
+	 if(y==0)
+		 return "No ContactDetails found with this name";
+	 else {
+		 addressBook.remove(c);
+		 addressBook.add(b);
+		 return "ContactDetails Updated";
+	 }
 		
+ }
+ public boolean removeContactDetails(String f_name, String l_name) {
+	 AddressBookStructure a=new AddressBookStructure("","","", "", "", "","", "");
+	 for(AddressBookStructure x:addressBook) {
+		 if(x.getfirstname().equalsIgnoreCase(f_name) && x.getlastname().equalsIgnoreCase(l_name)) {
+			 a=x;
+		 }
+	 }
+	 return addressBook.remove(a);
+ }
 	 }
 
 
