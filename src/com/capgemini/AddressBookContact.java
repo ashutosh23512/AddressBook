e1f0c783d42cd1a237a0a38cecbdd1f7f7965754
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-28 12:48:09 +0530

    [Ashutosh]Added:Edit contact

--- src/com/capgemini/AddressBookContact.java
+++ src/com/capgemini/AddressBookContact.java
@@ -4,12 +4,52 @@ public class AddressBookContact {
 
 	private ArrayList<AddressBookStructure> addressBook = new ArrayList<AddressBookStructure>();
 	
-	 public List<AddressBookStructure> getAddressBook(){
+	
+	 public ArrayList<AddressBookStructure> getAddressBook(){
 		 return this.addressBook;
 	 }
 	
 	 public void addContact(AddressBookStructure contactObj) {
 		 addressBook.add(contactObj);
 	 }
+	
+	 public boolean check(String name)
+	 {
+		 for(AddressBookStructure x: addressBook) {
+			 if(x.getfirstname().equalsIgnoreCase(name)) 
+				 return true;
+		 }
+		 return false;
+	 }
+	 
+	 public ArrayList<AddressBookStructure> viewContactDetails() {
+		 return addressBook;
+	 }
+	 
+		 
+	 public String updateContact(AddressBookStructure a) {
+		 int y=0;
+		 AddressBookStructure b = new AddressBookStructure("", "", "", "", "", "", "", "");
+		 AddressBookStructure c = new AddressBookStructure("", "", "", "", "", "", "", "");
+		
+		 for(AddressBookStructure x: addressBook) {
+			 if(x.getfirstname().equalsIgnoreCase(a.getfirstname()) && x.getlastname().equalsIgnoreCase(a.getlastname())) {
+			 c=x;
+			 b=a;
+			 y++;
+			 }
+		 }
+		 if(y==0)
+			 return "No contact found with this name";
+		 else {
+			 addressBook.remove(c);
+			 addressBook.add(b);
+			 return "Contact Updated";
+		 }
+			
+	 }
+	
+		
+	 }
 
-}
+
--- src/com/capgemini/AddressBookMain.java
+++ src/com/capgemini/AddressBookMain.java
@@ -5,10 +5,17 @@ public class AddressBookMain {
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to the Address Book");
+		
 		AddressBookContact newp = new AddressBookContact();
+		int flag=-1;
+		do {
 		AddressBookStructure ab = new AddressBookStructure();
+
 		Scanner s = new Scanner(System.in); 
 		System.out.println("Press 1 to Add New Contact ");
+		System.out.println("Press 2 to Edit Contact ");
+		System.out.println("Press 4 to View Address Book ");
+
 		int choice = s.nextInt();
 		Scanner s1 = new Scanner(System.in); 
 		switch(choice)
@@ -32,16 +39,74 @@ public class AddressBookMain {
 	        ab.setemail(s1.nextLine());
 	        newp.addContact(ab);
 	        System.out.println("Contact Created");
-	        System.out.println(ab.toString());
 	        break;
 	        
-	        default:
-	        	System.out.println("Incorrect Input");
-		}
-	}
+		case 2:
+			ArrayList<AddressBookStructure> l = new ArrayList<AddressBookStructure>();
+			l=newp.getAddressBook();
+			if(l.size() == 0) {
+			System.out.println("Empty Address Book");
+			break;
+			}
+			else
+			{
+			Scanner s2 = new Scanner(System.in);	
+			System.out.println("Enter First Name of the contact to be updated");
+			String name = s2.nextLine();
+			if(newp.check(name))
+			{
+			Scanner up = new Scanner(System.in);
+			System.out.println("Updated Contact details:");
+			System.out.println("Enter first name: ");
+			String fname1 = up.nextLine();
+			System.out.println("Enter last name: ");
+			String lname1 = up.nextLine();
+			System.out.println("Enter address: ");
+			String address1 = up.nextLine();
+			System.out.println("Enter city: ");
+			String city1 = up.nextLine();
+			System.out.println("Enter state: ");
+			String state1 = up.nextLine();
+			System.out.println("Enter zip: ");
+			String zip1 = up.nextLine();
+			System.out.println("Enter phone number: ");
+			String phonenumber=up.nextLine();
+			String phNo1 = up.nextLine();
+			System.out.println("Enter email: ");
+			String email1 = up.nextLine();
 
+			AddressBookStructure update = new AddressBookStructure(fname1, lname1, address1, city1, state1, zip1, phNo1, email1);
+			String updation = newp.updateContact(update);
+			System.out.println(updation);
 
+			
+			l=newp.viewContactDetails();
+			System.out.println("The updated Contact in Address Book is: ");
+			for(AddressBookStructure z:l) {
+			System.out.println(z);
+			}
+			}
+			else
+			{System.out.println("No such contact found");
+			break;}
+			}
+			break;
+			
+		
+					
+		case 4:
+			System.out.println(newp.getAddressBook());
+			break;
+				
+			
 	
+		case 0:
+			flag=0;
+        	System.out.println("Invalid Input");
+        	break;
+		}}
+		while(flag==-1);
+	}
 	}
 	
 
